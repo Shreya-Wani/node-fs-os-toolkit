40570414a25b934eaf200a92f0978608061288f3
Shreya Wani <shreyawani2006@gmail.com>
2026-01-18 19:05:45 +0530

    Create a folder in home directory

--- src/userLogger.js
+++ src/userLogger.js
@@ -5,4 +5,10 @@ import os from "os";
 //detect OS username
 const username = os.userInfo().username;
 
-//create folder 
+//Create a folder in home directory
+const homeDir = os.homedir();
+const logDir = path.join(homeDir, "my_app_logs");
+
+if (!fs.existsSync(logDir)){
+    fs.mkdirSync(logDir, { recursive: true });
+}
