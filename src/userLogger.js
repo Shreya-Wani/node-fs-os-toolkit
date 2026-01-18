334801574b3fd1d020465ef55c8abff7e486a27c
Shreya Wani <shreyawani2006@gmail.com>
2026-01-18 19:37:26 +0530

    add user-based daily logging system

--- src/userLogger.js
+++ src/userLogger.js
@@ -12,3 +12,15 @@ const logDir = path.join(homeDir, "my_app_logs");
 if (!fs.existsSync(logDir)){
     fs.mkdirSync(logDir, { recursive: true });
 }
+
+//Create daily log files (YYYY-MM-DD.log)
+export const logMessage = (message) => {
+    const today = new Date().toISOString().split('T')[0];
+    const logFile = path.log(logDir, `$(today).log`);
+
+    const time = new Date().toLocaleTimeString();
+    const log = `[${time}] ${message}\n`;
+
+    fs.appendFileSync(logFile, log);
+    console.log("Log saved");
+};
