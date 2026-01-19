5e24a750be1a18f32587798f9a4a6848b1c3aa94
Shreya Wani <shreyawani2006@gmail.com>
2026-01-19 17:16:14 +0530

    format snapshot data and append to snapshotFile

--- src/snapshot.js
+++ src/snapshot.js
@@ -11,4 +11,18 @@ export const takeSnapshot = () => {
   const user = os.userInfo().username;
   const cpuCores = os.cpus().length;
   const freeMemory = (os.freemem() / 1024 / 1024 / 1024).toFixed(2);
+
+   // Format snapshot data
+    const snapshot = `
+    Time: ${time}
+    User: ${user}
+    CPU Cores: ${cpuCores}
+    Free Memory: ${freeMemory} GB
+    ---------------------
+    `;
+
+    // Append snapshot to file (do not overwrite old data)
+    fs.appendFileSync(snapshotFile, snapshot);
+
+    console.log("System snapshot saved");
 }
