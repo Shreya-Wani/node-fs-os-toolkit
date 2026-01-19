eb28453aedc53c639e50482939cad692e9cf93b9
Shreya Wani <shreyawani2006@gmail.com>
2026-01-19 16:57:18 +0530

    raed all flies from source folder and copied it into backup folder

--- src/backup.js
+++ src/backup.js
@@ -9,4 +9,21 @@ export const backupFolder = (sourceDir) => {
     //create backup folder in project directory
     const backupDir = path.join(process.cwd(), `backup_${timestamp}`);
     fs.mkdirSync(backupDir);
+
+    //read all files from source directory
+    const files = fs.readFileSync(sourceDir);
+
+    let copiedCount = 0;
+
+    // copy each file to backup folder
+    files.forEach((file) => {
+        const srcPath = path.join(sourceDir, file);
+        const destPath = path.join(backupDir, file);
+
+        //copy only files (not folders)
+        if(fs.statSync(srcPath).isFile()) {
+            fs.copyFileSync(srcPath, destPath);
+            copiedCount++;
+        }
+    });
 }
